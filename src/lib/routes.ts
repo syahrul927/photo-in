f0e5524d0dedb18f6cc00e490a0aeceebe59f74d
Syahrul Ataufik <v-syahrul.ataufik@anteraja.id>
2025-03-24 13:56:46 +0700

    fix: add public route for invitation route

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,4 +1,5 @@
 export const LOGIN_ROUTE = "/auth/login";
+export const INVITATION_ROUTE = "/auth/invitation";
 export const NOT_FOUND = "/404";
 export const ROOT = "/";
-export const PUBLIC_ROUTES = [LOGIN_ROUTE, NOT_FOUND];
+export const PUBLIC_ROUTES = [LOGIN_ROUTE, INVITATION_ROUTE, NOT_FOUND];
