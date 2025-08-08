8d6a61b61d8056d0d774c97f6f13d5157cd312df
Syahrul Ataufik <syahrulataufikaja11@gmail.com>
2025-08-08 17:11:34 +0700

    feat: add public route to whitelist

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,5 +1,11 @@
 export const LOGIN_ROUTE = "/auth/login";
 export const INVITATION_ROUTE = "/auth/invitation";
+export const PUBLIC_CUSTOMER_ROUTE = "/public";
 export const NOT_FOUND = "/404";
 export const ROOT = "/";
-export const PUBLIC_ROUTES = [LOGIN_ROUTE, INVITATION_ROUTE, NOT_FOUND];
+export const PUBLIC_ROUTES = [
+  LOGIN_ROUTE,
+  INVITATION_ROUTE,
+  NOT_FOUND,
+  PUBLIC_CUSTOMER_ROUTE,
+];
